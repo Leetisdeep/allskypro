16905fdd3bbefc7bed1dc782ff595fb5a8a1d865
Leetisdeep <aaaooogjg@gmail.com>
2026-05-26 18:54:15 +0300

    refactoring

--- src/main/java/com/zoo/TgBotOnSpringBoot/listener/UpdateConsumer.java
+++ src/main/java/com/zoo/TgBotOnSpringBoot/listener/UpdateConsumer.java
@@ -1,17 +1,14 @@
 package com.zoo.TgBotOnSpringBoot.listener;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import com.zoo.TgBotOnSpringBoot.service.AnimalService;
+import com.zoo.TgBotOnSpringBoot.service.MessageService;
+import com.zoo.TgBotOnSpringBoot.service.UserService;
 
 /**
  * Класс UpdateConsumer отвечает за обработку обновлений от Telegram.
@@ -22,14 +19,35 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
     /**
      * Клиент для отправки запросов к Telegram API.
      */
-    private final TelegramClient telegramClient;
+    private final OkHttpTelegramClient telegramClient;
+    private final MessageService messageService;
 
     /**
-     * Конструктор UpdateConsumer.
-     * Инициализирует TelegramClient с заданным токеном (Сейчас токен тестовый и нерабочий).
+     * Конструктор UpdateConsumer для production.
+     * Инициализирует TelegramClient с заданным токеном.
      */
-    public UpdateConsumer() {
+    @Autowired
+    public UpdateConsumer(UserService userService, AnimalService animalService) {
         this.telegramClient = new OkHttpTelegramClient("8037617513:AAHJA2LiEpQdHvl6nUzBq3LSXLNwGbJi7gQ");
+        this.messageService = new MessageService(this.telegramClient, userService, animalService);
+    }
+
+    /**
+     * Конструктор UpdateConsumer для тестирования.
+     * Позволяет передавать мокированные зависимости.
+     */
+    public UpdateConsumer(OkHttpTelegramClient telegramClient, MessageService messageService) {
+        this.telegramClient = telegramClient;
+        this.messageService = messageService;
+    }
+
+    /**
+     * Конструктор UpdateConsumer для тестирования.
+     * Позволяет передавать только мокированный TelegramClient.
+     */
+    public UpdateConsumer(OkHttpTelegramClient telegramClient, UserService userService, AnimalService animalService) {
+        this.telegramClient = telegramClient;
+        this.messageService = new MessageService(telegramClient, userService, animalService);
     }
 
     /**
@@ -42,52 +60,23 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
     @Override
     public void consume(Update update) {
         if (update.hasMessage()) {
-            String messageText = update.getMessage().getText();
-            Long chatId = update.getMessage().getChatId();
-
-            if (messageText.equals("/start")) {
-                sendShelters(chatId);
-            }
+            messageService.handleMessage(update.getMessage());
+        } else if (update.hasCallbackQuery()) {
+            messageService.handleCallbackQuery(update.getCallbackQuery());
         }
     }
 
     /**
-     * Отправка пользователю приветственного сообщения и клавиатуры для выбора приюта.
-     * Клавиатура содержит кнопки "Приют для кошек" и "Приют для собак" (На данный момент не работают).
-     *
-     * @param chatId идентификатор чата, куда отправлять сообщения
+     * Геттер для TelegramClient
      */
-    private void sendShelters(Long chatId) {
-        SendMessage helloWorld = SendMessage.builder()
-                .text("*Приветствие*\nКнопочки нерабочие кстати")
-                .chatId(chatId)
-                .build();
-        SendMessage message = SendMessage.builder()
-                .text("Выберите приют")
-                .chatId(chatId)
-                .build();
+    public OkHttpTelegramClient getTelegramClient() {
+        return telegramClient;
+    }
 
-        var butt1 = InlineKeyboardButton.builder()
-                .text("Приют для кошек")
-                .callbackData("catShelter")
-                .build();
-        var butt2 = InlineKeyboardButton.builder()
-                .text("Приют для собак")
-                .callbackData("dogShelter")
-                .build();
-        List<InlineKeyboardRow> keyboardRows = List.of(
-                new InlineKeyboardRow(butt1),
-                new InlineKeyboardRow(butt2)
-        );
-        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup(keyboardRows);
-
-        message.setReplyMarkup(keyboard);
-
-        try{
-            telegramClient.execute(helloWorld);
-            telegramClient.execute(message);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+    /**
+     * Геттер для MessageService
+     */
+    public MessageService getMessageService() {
+        return messageService;
     }
 }
