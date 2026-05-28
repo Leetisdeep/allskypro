166b7e8976e6ee7ea3eeebaa40c47c91201ae49f
Leetisdeep <aaaooogjg@gmail.com>
2026-05-28 15:32:36 +0300

    yes

--- src/main/java/com/zoo/TgBotOnSpringBoot/repository/DailyReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zoo.TgBotOnSpringBoot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zoo.TgBotOnSpringBoot.model.DailyReport;
-
-public interface DailyReportRepository extends JpaRepository<DailyReport, Long> {
-    
-}
